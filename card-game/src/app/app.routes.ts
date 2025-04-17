3a4b8ad6f255eb5d95c7e2c2850969a816008caf
Sabrina <sabrinah_38@hotmail.fr>
2025-04-17 12:51:58 +0200

    remove home

--- card-game/src/app/app.routes.ts
+++ card-game/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 // app.routes.ts
 import { Routes } from '@angular/router';
-// import { HomeComponent } from './components/home/home.component';
 import { UserSignUpComponent } from './components/user-auth/user-signup/user-signup.component';
 import { UserSignInComponent } from './components/user-auth/user-signin/user-signin.component';
 
