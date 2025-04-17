0b2071f060bc25d29653f8751803232148c6528f
Sabrina <sabrinah_38@hotmail.fr>
2025-04-17 09:36:11 +0200

    maj title

--- card-game/src/app/app.component.spec.ts
+++ card-game/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'frontend-card' title`, () => {
+  it(`should have the 'card-game' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend-card');
+    expect(app.title).toEqual('card-game');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend-card');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, card-game');
   });
 });
